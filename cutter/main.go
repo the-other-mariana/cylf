0b20d7d6e408da983b968c155f6248fee69f55ed
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-07-08 12:56:55 -0500

    .cylf files created with file size limit
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- cutter/main.go
+++ cutter/main.go
@@ -6,47 +6,51 @@ import(
 	"math"
 	"strconv"
 	"os"
-	"string"
+	"strings"
 )
 
 func main(){
-	targetFile := "./seberg.mp4"
+	pwd, _ := os.Getwd()
+	targetFile := pwd + "/seberg-2019.mp4"
+	fmt.Printf("file: %v", targetFile)
+
+	
 	file, err := os.Open(targetFile)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("[ERROR] %v\n", err)
 		os.Exit(1)
 	}
 
-	defer file.close()
+	defer file.Close()
 
 	fileInfo, _ := file.Stat()
 	var fileSize int64 = fileInfo.Size()
-	const pieceSize = 99 * (1 << 20) // 99 MB
+	const pieceSize = 95 * (1 << 20) // 99 MB
 
 	numberOfPieces := uint64(math.Ceil(float64(fileSize) / float64(pieceSize)))
-	fmt.Println("[PROMPT] File will be cut into %d pieces", numberOfPieces)
+	fmt.Printf("[PROMPT] File will be cut into: %d pieces with size: %d bytes\n", numberOfPieces, pieceSize)
 	
 	for i := uint64(0); i < numberOfPieces; i++ {
-		currSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
-        currBuffer := make([]byte, partSize)
+		currSize := int(math.Min(pieceSize, float64(fileSize-int64(i*pieceSize))))
+        currBuffer := make([]byte, currSize)
 		file.Read(currBuffer)
 
-		nameParts := string.Split(targetFile('.'))
+		nameParts := strings.Split(targetFile, ".")
 		filename := nameParts[len(nameParts) - 2]
-		extension := nameParts[len(nameParts) - 1]
+		//extension := nameParts[len(nameParts) - 1]
 
 		// create file in disk
 		newFile := filename + "_" + strconv.FormatUint(i, 10) + ".cylf"
 		_, err := os.Create(newFile)
 
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("[ERROR] %v\n", err)
 			os.Exit(1)
 		}
 
 		// save buffer bytes into the created file
 		ioutil.WriteFile(newFile, currBuffer, os.ModeAppend)
-		fmt.Println("[SUCCESS] Piece: %v", newFile)
+		fmt.Printf("[SUCCESS] Piece: %v\n", newFile)
 	}
 	fmt.Println("[SUCCESS] file cut successfully")
 }
