eca509fe498890ff947f9d38e52526394ea2e238
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-11-15 20:23:50 -0600

    update: mb added to stats
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- src/mem/mem.go
+++ src/mem/mem.go
@@ -1,5 +1,7 @@
 package mem
 
+// taken from https://gist.github.com/j33ty/79e8b736141be19687f565ea4c6f4226
+
 import (
     "runtime"
     "fmt"
@@ -9,10 +11,10 @@ func PrintMemUsage() {
 	var m runtime.MemStats
 	runtime.ReadMemStats(&m)
 	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+	fmt.Printf("Alloc = %v MiB | %.2f MB", bToMb(m.Alloc), float64(bToMb(m.Alloc))*1.049)
+	fmt.Printf("\tTotalAlloc = %v MiB | %.2f MB", bToMb(m.TotalAlloc), float64(bToMb(m.TotalAlloc))*1.049)
+	fmt.Printf("\tSys = %v MiB | %.2f MB", bToMb(m.Sys), float64(bToMb(m.Sys))*1.049)
+	fmt.Printf("\tNumGC = %v | %.2f MB\n", m.NumGC, float64(m.NumGC)*1.049)
 }
 
 func bToMb(b uint64) uint64 {
