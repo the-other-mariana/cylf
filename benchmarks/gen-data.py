3550a987b6a7cb0bd60d8eaed7d77c4114a2eb4f
the-other-mariana <mariana.avalos.arce@gmail.com>
2022-11-27 19:42:11 -0600

    update: python code fills with all the *alloc* data by file
    Signed-off-by: the-other-mariana <mariana.avalos.arce@gmail.com>

--- benchmarks/gen-data.py
+++ benchmarks/gen-data.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt
+import os
+import re
+
 c = 256
 c_old = 256
 limit = 8192
@@ -16,5 +19,24 @@ for i in range(c_old, limit + 1, 16):
 plt.plot(xs, ys)
 plt.grid(True)
 
-plt.savefig('factor.png', dpi=500)
-plt.show()
+#plt.savefig('factor.png', dpi=500)
+#plt.show()
+
+files = [ f for f in os.listdir("./") if f.endswith(".txt") ] 
+data = { f.split('.')[0].split('-')[-1]:[] for f in files }
+print(data, files)
+for f in files:
+    print('Processing file:', f)
+    fopen = open(f, 'r')
+    lines = fopen.readlines()
+    for l in lines:
+        csv = l.split(',')
+        if 'Alloc' in l:
+            vals = csv[0].split(' = ')[-1].split(' | ')
+            for val in vals:
+                if re.match('.*(MB).*', val):
+                    num = val.strip().replace('MB', '')
+                    num = float(num)
+                    key = f.split('.')[0].split('-')[-1]
+                    data[key].append(num)
+print(data)
